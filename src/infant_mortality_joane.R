303382360441545f3282342678c2f9a2ff286494
JoaneElleouet <joane.elleouet@stats.govt.nz>
2021-09-30 16:58:12 +1300

    a bit more setting up

--- src/infant_mortality_joane.R
+++ src/infant_mortality_joane.R
@@ -1,8 +1,38 @@
 #' infant_mortality_joane.R
 
-# Set up
+# Set up -----------------------------------------------------------------------
 library(devtools)
 install_github("johnrbryant/bdefdata")
 library(bdefdata)
 deaths <- bdefdata::sweden_deaths
 births <- bdefdata::sweden_births
+# save data
+saveRDS(deaths, "./data/deaths.rds")
+saveRDS(births, "./data/births.rds")
+
+#-------------------------------------------------------------------------------
+
+library(dplyr)
+
+class(deaths)
+dim(births)
+dim(deaths)
+
+png("./out/distribution of births and deaths.png",height=600,width=450)
+par(mfrow=c(2,1))
+plot(density(births), col="blue", main = "births")
+plot(density(deaths), col="red", main = "deaths")
+dev.off()
+
+#assume everyone is born on Jan 1st?
+obs_mortality <- deaths/births
+hist(obs_mortality, main = "mortality")
+
+#reformat
+deaths_df <- as.data.frame(as.table(deaths))
+births_df <- as.data.frame(as.table(births))
+data <- full_join(deaths_df, births_df, by=c("county","time")) %>%
+  rename(deaths = Freq.x, births = Freq.y) %>%
+  mutate(county = as.character(county)) %>%
+  mutate(county_code = dense_rank(county))
+
---
+++ src/stan_model_joane_mlr_ARprior.R
@@ -0,0 +1,23 @@
+// binomial model with autoregressive structured prior for years and multilevel
+// structure for regions
+
+
+data {
+  int<lower=0> N;
+  vector[N] y;
+}
+
+// The parameters accepted by the model. Our model
+// accepts two parameters 'mu' and 'sigma'.
+parameters {
+  real mu;
+  real<lower=0> sigma;
+}
+
+// The model to be estimated. We model the output
+// 'y' to be normally distributed with mean 'mu'
+// and standard deviation 'sigma'.
+model {
+  y ~ normal(mu, sigma);
+}
+
