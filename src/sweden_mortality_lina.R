cd9cc49269cfdb786c0c629c5f524642feebfc29
Lina Berbesi <lberbesi@wprdrstudio01.stats.govt.nz>
2021-10-14 14:49:23 +1300

    Some initial changes

--- src/sweden_mortality_lina.R
+++ src/sweden_mortality_lina.R
@@ -1,3 +1,14 @@
+
+# clone the project before pushing data 
+#pwd
+#cd /home/STATSNZ/lberbesi//Network-Shares/U-Drive-SAS-02BAU/StdsMthds/"Statistical Methods"/Users/Lina/bayes_practical_problems/sweden_mortality/sweden_infant_mortality
+# git clone https://gitlabstats-prd/StatsMethods/bayesian-cop/projects/sweden_infant_mortality.git
+# git --init --initial-branch=main
+# git remote add origin https://gitlabstats-prd/StatsMethods/bayesian-cop/projects/sweden_infant_mortality/-/tree/master/src
+# git add sweden_mortality_lina.R
+# git commit  -n "Initial commit"
+# git push -u origin main
+
 library("devtools")
 library("bdefdata")
 suppressPackageStartupMessages(library("brms",quietly=TRUE))
@@ -25,12 +36,6 @@ names(deaths_melted)<-c("county","time","value")
 births_melted<-reshape2::melt(births,id="county")
 names(births_melted)<-c("county","time","value")
 
-i <- sapply(deaths_melted, is.factor)
-deaths_melted[i] <- lapply(deaths_melted[i], as.numeric)
-
-j <- sapply(births_melted, is.factor)
-births_melted[j] <- lapply(births_melted[j], as.numeric)
-
 data.table::setDT(deaths_melted)
 data.table::setDT(births_melted)
 
@@ -42,15 +47,15 @@ infant_dataset<-merge(births_melted,deaths_melted,by=c("county","time"))
 infant_dataset[, `:=` (
   delta = ifelse(time %in% "Stockholm", 1, 0),
   county = factor(county),
-  t = time - min(time) + 1)]
+  t = as.numeric(time) - min(as.numeric(time)) + 1)]
 
-infant_dataset[,-c("delta")]
+infant_dataset[,delta:=NULL]
 infant_dataset[order(county)]
 infant_dataset[,infant_mortality_rate:=1000*(deaths/births)]
 
 fit1 <-
   brms::brm(
-    infant_mortality_rate~ t + county,
+    infant_mortality_rate~ (1|t) + (1|county),
     data = infant_dataset,
     family = gaussian(),
     warmup = 500, 
@@ -65,11 +70,11 @@ prior_summary(fit1)
 fit1<-add_criterion(fit1,"loo")
 
 
-fit2 <-
+fit3 <-
   brms::brm(
-    infant_mortality_rate~ t + county,
+    deaths|trials(births)~ 1 + (1|time) + (1|county), # deaths=number of target events | total number of trials
     data = infant_dataset,
-    family = gamma(link=log),
+    family = binomial(link=logit),
     warmup = 500, 
     iter = 2000,
     chains=2,
@@ -77,8 +82,17 @@ fit2 <-
     seed = 123
   )
 
-summary(fit2)
-prior_summary(fit2)
-fit2<-add_criterion(fit2,"loo")
+summary(fit3)
+prior_summary(fit3)
 
-loo_compare(fit1,fit2,criterion="loo")
+fit3<-add_criterion(fit3,"loo")
+
+
+fit4<-glm(cbind(deaths,births-deaths)~1+time+county, # How to do it in the frequentist way?
+          family=binomial(logit),
+          data=infant_dataset)
+
+summary(fit4)
+
+
+
